2f083c7f218a710f7d019c831cf96ea2eebc1f27
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-16 17:15:12 +0200

    Create Home component

---
+++ src/pages/Home.js
@@ -0,0 +1,78 @@
+import React from "react";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function Home({ products }) {
+  return (
+    <main id="home">
+
+      {/* <div
+        id="carouselExampleInterval"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img src={products[1].image} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={products[2].image} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={products[3].image} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div> */}
+
+
+{/* <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active" data-bs-interval="10000">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item" data-bs-interval="2000">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+ */}
+
+    </main>
+  );
+}
+
+export default Home;

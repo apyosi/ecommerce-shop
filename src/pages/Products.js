bd0f22ed203f26839dcb599e4091d4f6b04ec2ea
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-16 17:15:29 +0200

    Create Products component

---
+++ src/pages/Products.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "../components/Card";
+
+function Products({products}) {
+  return (
+    <>
+      <main id="products" className="container-fluid">
+        <h1 className="text-center">Products</h1>
+        <hr />
+        <div className="container">
+          {/* <div className="row g-1 ">
+            {products.map((item) => (
+              <Card key={item.id} item={item} />
+            ))}
+          </div> */}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Products;

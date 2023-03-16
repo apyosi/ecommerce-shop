108b0c7b5d260cf3d7ace9fda5d13dcc3be6fec6
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-16 17:23:23 +0200

    Import useEffect, useState & fetch the products(fakestoreapi.com)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
@@ -7,14 +8,27 @@ import NotFound from "./pages/NotFound";
 import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    // console.log(data);
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return <div className="App">
     <Router>
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route index element={<Home products={products} />} />
         <Route path="products">
-          <Route index element={<Products />} />
-          <Route path=":id" element={<Product />} />
+          <Route index element={<Products products={products} />} />
+          <Route path=":id" element={<Product products={products} />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Route>

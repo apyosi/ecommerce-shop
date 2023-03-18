c304611af1779a0400675db2d37dd8b9588a4da3
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-18 18:33:28 +0200

    update the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Card = ({ item }) => {
+const Card = ({ item, category }) => {
+
   return (
-    <article className="card col-md-4 col-sm-6 border-0" >
-      <div className="text-center w-100 p-2 border border-0 border-light ">
-        <Link to={`${item.id}`}>
-          <img src={item.thumbnail} className="img-thumbnail border border-1 border-white" width="300px" height="300px" alt={item.id} />
-        </Link>
-        <Link to={`${item.id}`}>
-          <h3 className="card-title">{item.title}</h3>
-        </Link>
-        <p className="card-text"><b>Price:</b> ${item.price}</p>
-        <p className="card-text"><b>Category:</b> {item.category}</p>
-        <p className="card-text"><b>Brand:</b> {item.brand}</p>
-        <p className="card-text">{item.description}</p>
+    <article className="border-black border-solid border">
+      <div className="flex flex-col justify-items-end align-align-items-center">
+        {category && (
+          <>
+            <Link to={`${category}/${item.id}`}>
+              <img
+                src={item.thumbnail}
+                className="overflow-hidden rounded-3xl border border-black border-solid"
+                width="300px"
+                height="300px"
+                alt={item.id}
+              />
+            </Link>
+            <Link to={`${category}/${item.id}`}>
+              <h3 className="card-title">{item.title}</h3>
+            </Link>
+          </>
+        )}
+        {!category && (
+            <>
+              <Link to={`${item.id}`}>
+                <img
+                  src={item.thumbnail}
+                  className="img-thumbnail border border-1 border-white"
+                  width="300px"
+                  height="300px"
+                  alt={item.id}
+                />
+              </Link>
+              <Link to={`${item.id}`}>
+                <h3 className="card-title">{item.title}</h3>
+              </Link>
+            </>
+          )
+        }
+        <p className="card-text">
+           ${item.price}
+        </p>
+        {/* <p className="card-text">
+          <b>Category:</b> {item.category}
+        </p> */}
+        <p className="card-text">
+          <b>Brand:</b> {item.brand}
+        </p>
+        {/* <p className="card-text">{item.description}</p> */}
       </div>
     </article>
   );

6eb5c9693095b1be4ea3c32e88686f4216022804
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-03-23 16:01:17 +0000

    Added hover to icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,27 +10,27 @@ const Footer = () => {
        {/* Social Icons */}
       <div className="socialLinks md:mr-7">
        <ul className="icons flex">
-        <li className="text-4xl md:mx-4">
+        <li className="text-4xl md:mx-4 hover:-translate-y-3 duration-300">
           <Link to="https://instagram.com" aria-hidden="true" target="_blank" rel="noopener noreferrer">
            <FaInstagramSquare/>
          </Link>
         </li>
-        <li className="text-4xl md:mx-4">
+        <li className="text-4xl md:mx-4 hover:-translate-y-3 duration-300 ">
           <Link to="https://www.facebook.com" aria-hidden="true" target="_blank" rel="noopener noreferrer">
             <FaFacebookSquare></FaFacebookSquare>
           </Link>
         </li>
-        <li className="text-4xl md:mx-4">
+        <li className="text-4xl md:mx-4 hover:-translate-y-3 duration-300">
           <Link to="https://www.linkedin.com" aria-hidden="true"  target="_blank" rel="noopener noreferrer">
             <FaLinkedin/>
           </Link>
         </li>
-        <li className="text-4xl md:mx-4">
+        <li className="text-4xl md:mx-4 hover:-translate-y-3 duration-300">
           <Link to="https://twitter.com" aria-hidden="true" target="_blank" rel="noopener noreferrer">
             <FaTwitter/>
           </Link>
         </li>
-        <li className="text-4xl md:mx-4"><Link to="https://tiktok.com" aria-hidden="true" rel="noopener noreferrer" target="_blank">
+        <li className="text-4xl md:mx-4 hover:-translate-y-3 duration-300"><Link to="https://tiktok.com" aria-hidden="true" rel="noopener noreferrer" target="_blank">
          <FaTiktok/>
         </Link></li>
        </ul>

529d45689e081a847309248e8bd5f97905ff0c14
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-24 18:49:08 +0200

    thank page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import Card from "../components/Card";
+import { Link } from "react-router-dom";
+// import Card from "../components/Card";
 import Confetti from '../components/Confetti'
 
 
@@ -11,14 +12,33 @@ function Products({products}) {
 
   return (
     <>
-      <main id="products" className="container-fluid mt-[140px] md:mt-[110px]">
-        <h1 className="text-center">Products</h1>
-        <hr />
+<main
+        id="thanks"
+        className="container-fluid h-[90%] mt-[140px] md:mt-[110px] border-0 shadow-xl"
+      >
         <div className="container">
-          <div className="row g-1 ">
-            {products.map((item) => (
-              <Card key={item.id} item={item} />
-            ))}
+          <div className="success-wrapper">
+            <div className="flex flex-col justify-center align-content-center h-[600px] shadow-xl">
+              <h2 className="text-5xl mb-10">Thank you for your order!</h2>
+              <p className="text-3xl mb-5">
+                Check your email inbox for the receipt.
+              </p>
+              <p className="text-3xl mb-14">
+                If you have any questions, please email us: <br />
+                <a className="email" href="mailto:order@example.com">
+                  order@example.com
+                </a>
+              </p>
+              <Link to="/">
+                <button
+                  type="button"
+                  width="300px"
+                  className="p-4 mb-[200px] bg-yellow-400 text-3xl border-0  shadow-xl"
+                >
+                  Continue Shopping
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </main>
